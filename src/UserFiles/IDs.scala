713678331ca971bf5b59f8edadddf7e93fbd1268
Jake Fitzpatrick <jefitzpa@gmail.com>
2019-02-26 20:58:55 -0500

    -Adapted almost all functions to work with classes -Created a class for the player with limited functionality -Can't seem to figure out how to create unique Ids
     Jake Fitzpatrick

--- src/UserFiles/IDs.scala
+++ src/UserFiles/IDs.scala
@@ -7,18 +7,27 @@ object IDs {
     val id = CheckForID()
 
     val user = new Player(username, location, id, 0)
+    println(username)
+    println(location)
+    println(id)
   }
 
   def findSuitableSpawn(): List[Int] = {
+    val r = scala.util.Random
 
+    var x: Int = r.nextInt(100)
+    var y: Int = r.nextInt(100)
+
+    List(x,y)
   }
 
   def CheckForID(): Int = {
-
+    0
   }
 
 
   def main(args: Array[String]): Unit = {
+    AddUser("Jake")
   }
 
 }
