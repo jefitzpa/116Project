4e00fe7780e5ff84cf40264e4a711f5e3bb42f90
Jake Fitzpatrick <jefitzpa@gmail.com>
2019-04-19 09:48:03 -0400

    -Continued Method editing and creation for Web Gui MMP Support -Added some more socket functions to server -Began adding request to the Web Gui
     Jake Fitzpatrick
    
    Signed-off-by: Jake Fitzpatrick <jefitzpa@gmail.com>

--- src/Networking/Server.py
+++ src/Networking/Server.py
@@ -1,6 +1,6 @@
 import eventlet
 import socket
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from flask_socketio import SocketIO
 
 eventlet.monkey_patch()
@@ -8,8 +8,8 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 socket_server = SocketIO(app)
 
-usernameToSid = {}
-sidToUsername = {}
+userIDToSid = {}
+sidToUserID = {}
 
 scala_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scala_socket.connect(('localhost', 8000))
@@ -18,6 +18,27 @@ scala_socket.connect(('localhost', 8000))
 @app.route("/")
 def index():
     return send_from_directory('WebGui', 'Index.html')
+
+
+@socket_server.on('register')
+def RegisterPlayer():
+    Id = getFromScala({"UserId": 0, "action": "createPlayer"})
+
+    userIDToSid[Id] = request.sid
+    sidToUserID[request.sid] = Id
+    print(Id + " connected")
+    message = {"userID": Id, "action": "connected"}
+    SendToScala(message)
+
+
+def SendToScala(message):
+
+
+def getFromScala(request):
+    return 0
+
+
+socket_server.run(app, port=8080)
 
 
 
