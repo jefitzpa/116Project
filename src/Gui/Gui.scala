739756b259ac1cc10c03b96d9909b20322bb0714
Jake Fitzpatrick <jefitzpa@gmail.com>
2019-05-05 14:13:37 -0400

    -Nearly there
    Jake Fitzpatrick
    
    Signed-off-by: Jake Fitzpatrick <jefitzpa@gmail.com>

--- src/Gui/Gui.scala
+++ src/Gui/Gui.scala
@@ -5,52 +5,69 @@ import io.socket.emitter.Emitter
 import javafx.scene.control.Button
 import javafx.scene.input.{KeyCode, KeyEvent}
 import javafx.scene.text.Text
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.control.TextField
 import scalafx.scene.layout.StackPane
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Rectangle}
+import scalafx.scene.shape.Rectangle
 import scalafx.scene.{Group, Scene}
 
-class HandleMessagesFromPython() extends Emitter.Listener {
+class HandleMessagesFromPython(gameState: GameState) extends Emitter.Listener {
   override def call(objects: Object*): Unit = {
     val message = Json.parse(objects.apply(0).toString)
 
-    Gui.playersOnline = (message \ "players").as[Map[String, Map[String, String]]]
-    Gui.Coins = (message \ "coins").as[Map[String, Map[String, String]]]
-
-    Gui.UpdateGame()
+    gameState.gs = message
   }
 }
 
-object Gui extends JFXApp {
+class GameState(var gs: JsValue)
 
-  var socket: Socket = IO.socket("http://localhost:8080/")
-  socket.connect()
+object Gui extends JFXApp {
 
   var playersOnline: Map[String, Map[String, String]] = Map()
   var Coins: Map[String, Map[String, String]] = Map()
+
+  var connected = false
 
   var graphics : Group = new Group {}
 
   val windowWidth : Double = 500
   val windowHeight : Double = 500
 
+  var socket: Socket = IO.socket("http://localhost:8080/")
+
+  val gameState = new GameState(Json.toJsObject(Map("players" -> playersOnline, "coins"-> Coins)))
+
+  socket.on("gameState", new HandleMessagesFromPython(gameState))
+
+  socket.connect()
+
   def Connect(name: String): Unit = {
     socket.emit("register", name, socket.id())
+    this.connected = true
   }
 
-  socket.on("gameState", new HandleMessagesFromPython)
+  def UpdateGame(coins: Map[String, Map[String, String]], players: Map[String, Map[String, String]]): Unit = {
 
-  def UpdateGame(): Unit = {
+    Coins = coins
+    playersOnline = players
 
-    Gui.graphics.children.clear()
+    graphics.children.clear()
+
+    if (connected) {
+      amountOfCoins.setText("Coins: " + playersOnline(socket.id())("coins"))
+    }
+    graphics.children.addAll(amountOfCoins)
 
     for (player <- playersOnline.keys){
       createPlayer(playersOnline(player))
+    }
+
+    for (coin <- Coins.keys){
+      createCoin(Coins(coin))
     }
   }
 
@@ -65,74 +82,43 @@ object Gui extends JFXApp {
       width = 18
       height = 18
     }
-    val plyName: Text = new Text(location.head.toDouble - 12, location.tail.head.toDouble - 15, name)
+    val plyName: Text = new Text(location.head.toDouble, location.tail.head.toDouble - 10, name)
 
     graphics.children.addAll(ply, plyName)
   }
 
+  def createCoin(coin9: Map[String, String]): Unit = {
+    val coin = new Rectangle(){
+      x = coin9("x").toDouble
+      y = coin9("y").toDouble
+      fill = Color.Yellow
+      width = 10
+      height = 10
+    }
 
-  //end of new code
+    graphics.children.addAll(coin)
+  }
 
-  var coins = 0
+  var keyStates: Map[String, Boolean] = Map("w" -> false, "a" -> false, "s" -> false, "d" -> false)
+
+  def MakeKeyRequest(keyCode: KeyCode): Unit = {
+    keyCode.getName match {
+      case "W" => keyStates = Map("w" -> true, "a" -> false, "s" -> false, "d" -> false)
+      case "A" => keyStates = Map("w" -> false, "a" -> true, "s" -> false, "d" -> false)
+      case "S" => keyStates = Map("w" -> false, "a" -> false, "s" -> true, "d" -> false)
+      case "D" => keyStates = Map("w" -> false, "a" -> false, "s" -> false, "d" -> true)
+      case _ => println("Not an error")
+    }
+    socket.emit("keyStates", Json.toJson(keyStates))
+  }
 
   var Skin : Color = Color.Red
-
-  var playerXvelo = 0.0
-  var playerYvelo = 0.0
-
-  //Create player label
-  val playerName = new Text
-  playerName.setTranslateY(-20)
-
-  //initializes player graphic
-  val player: Circle = new Circle {
-    radius = 10
-    centerX= 400
-    centerY = 300
-    fill = Skin
-  }
-
-  //group player name and player
-  val PlayerStack = new StackPane {
-    translateX = player.centerX.value
-    translateY = player.centerY.value
-  }
-  PlayerStack.getChildren.addAll(player, playerName)
-
-  //simple coin, will need to be revised
-  val coin = new Circle()
-  coin.setCenterX(400)
-  coin.setCenterY(100)
-  coin.setRadius(9)
-  coin.fill = Color.Yellow
 
   //start button and name textfield
   val start = new Button {
     setText("Start")
     setOnAction(new StartButton)
     setPrefSize(500, 100)
-  }
-  var SkinRed = new Button {
-    setText("Red")
-    setOnAction(new SkinActionRed)
-    setPrefSize(100, 100)
-    setTranslateY(120)
-  }
-
-  var SkinBlue = new Button {
-    setText("Blue")
-    setOnAction(new SkinActionBlue)
-    setPrefSize(100, 100)
-    setTranslateX(-200)
-    setTranslateY(120)
-  }
-
-  var SkinGreen = new Button {
-    setText("Green")
-    setOnAction(new SkinActionGreen)
-    setPrefSize(100, 100)
-    setTranslateX(200)
-    setTranslateY(120)
   }
 
   val inputName = new TextField
@@ -146,36 +132,17 @@ object Gui extends JFXApp {
 
   val amountOfCoins = new Text {
     setText("Coins: " + "0")
-    setTranslateX(650)
+    setTranslateX(385)
     setTranslateY(25)
     setStyle("-fx-font: 30 ariel;")
   }
-  StartStack.getChildren.addAll(start, inputName, SkinRed, SkinBlue, SkinGreen)
+  StartStack.getChildren.addAll(start, inputName)
 
-  graphics.children.addAll(amountOfCoins, coin, StartStack)
-
-  def MakeKeyRequest(keyCode: KeyCode): Unit = {
-    keyCode.getName match {
-      case "W" => playerYvelo = -10
-      case "A" => playerXvelo = -10
-      case "S" => playerYvelo = 10
-      case "D" => playerXvelo = 10
-      case "Enter" => new StartButton
-      case _ => println("Not an error")
-    }
-  }
-
-  def placeNewCoin(): Unit ={
-    val r = scala.util.Random
-    coin.setCenterX(r.nextInt(750))
-    coin.setCenterY(r.nextInt(550))
-    graphics.children.removeAll(PlayerStack,amountOfCoins)
-    graphics.children.addAll(coin, PlayerStack, amountOfCoins)
-  }
+  graphics.children.addAll(amountOfCoins, StartStack)
 
   this.stage = new PrimaryStage {
     this.title = "Coin Collector"
-    scene = new Scene(windowWidth, windowHeight) {
+    scene = new Scene(windowWidth, windowHeight + 10) {
       content = List(graphics)
       fill = Color.rgb(164, 166, 168)
 
@@ -185,20 +152,10 @@ object Gui extends JFXApp {
 
 
     val update: Long => Unit = (time: Long) => {
-      PlayerStack.translateX.value += playerXvelo
-      PlayerStack.translateY.value += playerYvelo
-
-      playerYvelo = 0.0
-      playerXvelo = 0.0
-
-     if (Math.pow(player.centerX.value - coin.centerX.value,2) + Math.pow(player.centerY.value - coin.centerY.value, 2) < 361){
-       coins += 1
-       graphics.children.removeAll(coin)
-       placeNewCoin()
-     }
-      player.setCenterX(PlayerStack.translateX.value + 10)
-      player.setCenterY(PlayerStack.translateY.value + 10)
-      amountOfCoins.setText("Coins: " + coins.toString)
+      if (connected == true) {
+        UpdateGame((gameState.gs \ "coins").as[Map[String, Map[String, String]]],
+          (gameState.gs \ "players").as[Map[String, Map[String, String]]])
+      }
     }
     AnimationTimer(update).start()
   }
